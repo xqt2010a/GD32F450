087c5de7eab652b472fc92ff023a83c6f5e4342b
labc <2367334102@qq.com>
2018-01-04 00:46:03 +0800

    add queue demo in main.

--- STM32F103/210_Queue/user/main.c
+++ STM32F103/210_Queue/user/main.c
@@ -3,20 +3,28 @@
 #include "queue.h"
 
 #define PRT(...) SEGGER_RTT_printf(0, __VA_ARGS__)
-#define Queue_Len   6
+#define Queue_Len   20
 
-uint8_t Buf[10]="hello";
-
+uint8_t Buf[20]="Labc New Date!\r\n";
+uint8_t Buf_T;
 void main(void)
 {
-    pQueue Q;
+    uint8_t i;
+    Queue Q;
     
     SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_SKIP);
     PRT("hello world!\r\ntoday is :%s,%s\r\n",__DATE__, __TIME__);
     
-    GreateQueue(Q, Queue_Len);
+    GreateQueue(&Q, Queue_Len);
     while(1)
     {
-        
+        for(i=0; i<Queue_Len; i++){
+            EnQueue(&Q, Buf[i]);    
+        }
+        for(i=0; i<Queue_Len; i++){
+            DeQueue(&Q,&Buf_T);
+            PRT("%c",Buf_T);
+        }
+        while(1);
     }
 }
