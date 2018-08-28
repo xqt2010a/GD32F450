1a11f49c82e46217cee0968fcd2d9832fc1f2db4
xqt2010a <2367334102@qq.com>
2018-08-28 19:44:08 +0800

    add dma.

--- JX/107_dma/user/main.c
+++ JX/107_dma/user/main.c
@@ -1,4 +1,5 @@
 #include "jx_uart.h"
+#include "jx_dma.h"
 #include "stdio.h"
 
 #define JX_W4(x)    (*(unsigned int *)(x))
@@ -30,11 +31,21 @@ void smu_init(void)
 	udelay(10000);
 }
 
+unsigned char Dst_buf[32]="hello world ! code by labc.";
+unsigned char Src_buf[32];
+
 void main(void)
 {
     smu_init();
+    DMA_Struct dma_s;
+    dma_s.dma = DMA0;
+    dma_s.ch = CH1;
+    dma_s.width = WIDTH_16;
+    dma_s.size = SIZE_8;
+    dma_s.dst = (uint32_t)Dst_buf;
+    dma_s.src = (uint32_t)Src_buf;
+    dma_s.len = 2;
     
-    uart_init(115200);
-    printf("hello world!\r\n");
+    dma_m2m(&dma_s);
     while(1);
 }
