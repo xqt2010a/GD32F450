ffe338f04c8581782d33794ccda466facc6bc8a0
labc <2367334102@qq.com>
2018-12-03 14:12:12 +0800

    edit uart_init.

--- JX/103_sdio/user/main.c
+++ JX/103_sdio/user/main.c
@@ -1,11 +1,14 @@
 #include "jx_uart.h"
 #include "stdio.h"
 #include "mmc_boot.h"
+#include "system.h"
 
 
 void main(void)
 {
-    uart_init(115200);
+    uint32_t sysclk;
+    sysclk = get_sysclk();
+    uart_init(115200, sysclk);
     printf("mmc read write test!\r\n");
     mmc_test();
     while(1);
