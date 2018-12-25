3e5cf1a074c23c69b6c21859c24475343da99994
labc <2367334102@qq.com>
2018-12-25 19:36:30 +0800

    remove no used code.

--- JX/bsp/ip/jx_int.c
+++ JX/bsp/ip/jx_int.c
@@ -2,40 +2,9 @@
 #include "jx_int.h"
 #include <intrinsics.h>
 
-
 #define INT_NUM     229
 
-
 static void (*IRQ_HandleFunc[INT_NUM])(void);
-
-static unsigned int step_mask(int step)
-{
-	int i;
-	unsigned int mask=0;
-
-	/* We assume that step is between 1 and 31. No check here.*/
-	for(i = 0;i < step; i++){
-		mask |= 0x1 << i;
-	}
-	return mask;
-}
-
-void gic_reg_op(int base, int step, int val, unsigned int irqnum, int op)
-{
-	unsigned int addr, offset, tmpval;
-	addr = base + (irqnum*step/32)*4;
-	offset = (irqnum*step)%32;
-
-	tmpval = JX_WR4(addr);
-	if(op) {
-		tmpval |= (val&step_mask(step)) << offset;
-	} else {
-		tmpval &= ~(step_mask(step) << offset);
-	}
-
-	JX_WR4(addr) = tmpval;
-	return ;
-}
 
 void IRQ_SetEnable(void)
 {
@@ -45,22 +14,6 @@ void IRQ_SetEnable(void)
 
 void IRQ_SetNum(unsigned int irqnum)
 {
-//    int op = 1;
-
-    /* DETECT 0x03: high level trigger*/
-    //gic_reg_op(INT_ICDICFR, 0x2, 0x3, irqnum, op);
-    /* Priority */
-    //gic_reg_op(INT_ICDIPR, 0x8, 0xA0, irqnum, op);
-    /* Target CPU */
-    //gic_reg_op(INT_ICDIPTR, 0x8, 0x01, irqnum, op);
-    /* Enable Set */
-    //gic_reg_op(INT_ICDISER, 0x1, 0x1, irqnum, op);
-    
-    /* DETECT 0x03: high level trigger*/
-//    gic_reg_op(SYS_GIC_BASE+0x1C00, 0x2, 0x3, irqnum, op);
-//    gic_reg_op(SYS_GIC_BASE+0x1400, 0x8, 0xA0, irqnum, op);
-//    gic_reg_op(SYS_GIC_BASE+0x1800, 0x8, 0x01, irqnum, op);
-//    gic_reg_op(SYS_GIC_BASE+0x1100, 0x1, 0x1, irqnum, op);
     // Interrupt Configuration Registers, DETECT 0x03: high level trigger
     register_write((uint32_t)&GICD_ICFGR(irqnum/32), 2, irqnum%32, 0x3);
     // Priority, 10
@@ -77,16 +30,10 @@ void IRQ_Init(void)
     GICD_CTRL = 0;     //disable seclect cpu interface
     GICC_CTLR = 0;     //disable cpu interrupt
     
-//    for(i = 0; i< 1020; i++){
-//        //gic_reg_op(INT_ICDISER, 0x1, 0x1, i, 0);
-//        gic_reg_op(SYS_GIC_BASE+0x1100, 0x1, 0x1, i, 0);
-//    }
     for(i=32; i<INT_NUM; i++){
         register_write((uint32_t)&GICD_ISENABLER(i/32), 1, i%32, 0);
     }
-    //GICD_CTRL = 0x3;   //enable non-secure and secure
     GICC_PMR = 0xff; //Priority Mask Register, 0xff handle all interrupt
-    //IRQ_SetEnable();
 }
 
 int IRQ_Register(unsigned int irqnum, void f(void))
@@ -98,10 +45,7 @@ int IRQ_Register(unsigned int irqnum, void f(void))
 
 uint32_t IRQ_GetID(void)
 {
-    uint32_t id;
-    id = GICC_IAR;
-    id = id & 0x3FF;
-    return id;
+    return GICC_IAR & 0x3FF;
 }
 
 void IRQ_Handler(void)
@@ -109,9 +53,9 @@ void IRQ_Handler(void)
     uint32_t id;
     id = IRQ_GetID();
     IRQ_HandleFunc[id]();
-    GICC_EOIR = id;   //end of interrupt register
+    GICC_EOIR = GICC_EOIR & ((~0x3FF)|id);   //end of interrupt register
     /* Ensure the write takes before re-enabling interrupts. */
-	__DSB();
-	__ISB();
-    __enable_irq();
+	//__DSB();
+	//__ISB();
+    //__enable_irq();
 }
