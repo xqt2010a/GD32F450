48c8962d047926440a132ef0df2385d8adbc0fa7
labc <2367334102@qq.com>
2018-12-27 20:46:47 +0800

    edit uart in sdio project.

--- JX/103_sdio_small/user/main.c
+++ JX/103_sdio_small/user/main.c
@@ -3,12 +3,19 @@
 #include "mmc_boot.h"
 #include "system.h"
 
+UART_InitTypeDef uart;
+
+void uart_struct_init(void)
+{
+    uart.ch = UART1;
+    uart.bandrate = 115200;
+    uart.int_mask = 0;
+}
 
 void main(void)
 {
-    uint32_t sysclk;
-    sysclk = get_sysclk();
-    uart_init(115200, sysclk);
+    uart_struct_init();
+    uart_init(&uart);
     printf("mmc read write test!\r\n");
     mmc_test();
     while(1);
